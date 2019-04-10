d31091119bf05b44b3036f8364a7a195ab2c4b3d
ravipatel <102030@@>
2019-04-10 15:47:14 +0530

    init Comment

---
+++ app/src/main/java/com/example/googlesignindemo/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.googlesignindemo;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    SignInButton signInButton;
+    GoogleSignInClient mGoogleSignInClient;
+    Button btn_sign_out;
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        signInButton = findViewById(R.id.btn_sign_in);
+        btn_sign_out = findViewById(R.id.btn_sign_out);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        findViewById(R.id.btn_sign_in).setOnClickListener(this);
+        findViewById(R.id.btn_sign_out).setOnClickListener(this);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_sign_in: {
+                signIn();
+                break;
+            }
+            case R.id.btn_sign_out:
+                signOut();
+                break;
+            // ...
+        }
+    }
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                        updateUI(null);
+                    }
+                });
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 111);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 111) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        TextView name = findViewById(R.id.txtName);
+        ImageView imageView = findViewById(R.id.imgProfilePic);
+        TextView email = findViewById(R.id.txtEmail);
+        LinearLayout llProfile = findViewById(R.id.llProfile);
+        if (account != null) {
+            findViewById(R.id.btn_sign_in).setVisibility(View.GONE);
+            findViewById(R.id.btn_sign_out).setVisibility(View.VISIBLE);
+            findViewById(R.id.llProfile).setVisibility(View.VISIBLE);
+
+            Picasso.with(this).load(account.getPhotoUrl()).into(imageView);
+            Log.e(TAG, "updateUI: "+account.getPhotoUrl() );
+            name.setText(account.getDisplayName());
+            email.setText(account.getEmail());
+        } else {
+            findViewById(R.id.llProfile).setVisibility(View.GONE);
+            findViewById(R.id.btn_sign_in).setVisibility(View.VISIBLE);
+            findViewById(R.id.btn_sign_out).setVisibility(View.GONE);
+        }
+    }
+}
